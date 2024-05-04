bc49e3df19e22c87efccf2444c1f81aafac87c2a
isanasan <yfcgpsebp@gmail.com>
2024-05-04 12:39:20 +0900

    add: test

---
+++ playground/tests/QuerierTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+use Isanasan\SqlcGenPhpPlayground\Querier;
+use PHPUnit\Framework\TestCase;
+
+class QuerierTest extends TestCase
+{
+    public function testGetUser()
+    {
+        $dsn = 'mysql:host=127.0.0.1;dbname=test;charset=utf8mb4';
+        $username = 'root';
+        $password = null;
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        $pdo = new PDO($dsn, $username, $password, $options);
+
+        $querier = new Querier($pdo);
+
+        $this->assertSame(
+            [
+                "id" => 1,
+                "name" => "hoge",
+                "email" => "test@example.com",
+                "age" => 20
+            ],
+            $querier->getUser(1)
+        );
+    }
+}
