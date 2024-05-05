184320da4fe2d3e625c22813b2bd2125861baf1f
isanasan <yfcgpsebp@gmail.com>
2024-05-05 10:52:50 +0900

    refact: use setUp

--- playground/tests/QuerierTest.php
+++ playground/tests/QuerierTest.php
@@ -1,14 +1,17 @@
 <?php
 
-
 use Isanasan\SqlcGenPhpPlayground\Querier;
 use Isanasan\SqlcGenPhpPlayground\User;
 use PHPUnit\Framework\TestCase;
 
 class QuerierTest extends TestCase
 {
-    public function testGetUser()
+    private Querier $querier;
+
+    public function setUp(): void
     {
+        parent::setUp();
+
         $dsn = 'mysql:host=127.0.0.1;dbname=test;charset=utf8mb4';
         $username = 'root';
         $password = null;
@@ -18,8 +21,11 @@ class QuerierTest extends TestCase
         ];
         $pdo = new PDO($dsn, $username, $password, $options);
 
-        $querier = new Querier($pdo);
+        $this->querier = new Querier($pdo);
+    }
 
+    public function testGetUser()
+    {
         $this->assertEquals(
             new User(...[
                 "id" => 1,
@@ -27,23 +33,12 @@ class QuerierTest extends TestCase
                 "email" => "test@example.com",
                 "age" => 20
             ]),
-            $querier->getUser(1)
+            $this->querier->getUser(1)
         );
     }
 
     public function testGetNoUser()
     {
-        $dsn = 'mysql:host=127.0.0.1;dbname=test;charset=utf8mb4';
-        $username = 'root';
-        $password = null;
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-        $pdo = new PDO($dsn, $username, $password, $options);
-
-        $querier = new Querier($pdo);
-
-        $this->assertNull($querier->getUser(2));
+        $this->assertNull($this->querier->getUser(2));
     }
 }
